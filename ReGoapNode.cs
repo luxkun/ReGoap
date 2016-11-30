f542039b0cbc733318b14ca10c2bd7922f1b119a
Lukian <luciano.ferraro@gmail.com>
2016-11-30 10:12:32 +0100

    ReGoapNode and ReGoapState now have their own files. ReGoapState add operator now returns a new state (used to change the first state).

---
+++ ReGoapNode.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ReGoapNode : INode<ReGoapState>
+{
+    private readonly int cost;
+    private readonly IGoapPlanner planner;
+    private readonly ReGoapNode parent;
+    private readonly IReGoapAction action;
+    private readonly ReGoapState state;
+    private readonly ReGoapState goal;
+    private readonly int g;
+    private readonly int h;
+
+    private readonly int heuristicMultiplier = 1;
+    private readonly bool backwardSearch;
+
+    public ReGoapNode(IGoapPlanner planner, ReGoapState newGoal, ReGoapNode parent, IReGoapAction action)
+    {
+        this.planner = planner;
+        this.parent = parent;
+        this.action = action;
+        goal = (ReGoapState)newGoal.Clone();
+        backwardSearch = planner.GetSettings().backwardSearch;
+
+        if (this.parent != null)
+        {
+            state = this.parent.GetState();
+            // g(node)
+            g = parent.GetPathCost();
+        }
+        else
+        {
+            state = planner.GetCurrentAgent().GetMemory().GetWorldState();
+        }
+        //state = (ReGoapState)state.Clone(); // no need anymore since ReGoapState add operator now returns a new state
+        if (action != null)
+        {
+            var effects = (ReGoapState)action.GetEffects(goal).Clone();
+            state += effects;
+            g += action.GetCost(goal);
+        }
+        // missing states from goal
+        // h(node)
+        // we calculate this after getting the heuristic value so actions that gives us goal state will go first
+        if (backwardSearch && action != null)
+        {
+            var diff = new ReGoapState();
+            // backward search does NOT support negative preconditions
+            action.GetPreconditions(goal)
+                .MissingDifference(state, ref diff, predicate: (pair, otherValue) => !pair.Value.Equals(false));
+            goal += diff;
+        }
+        // f(node) = g(node) + h(node)
+        cost = g + h * heuristicMultiplier;
+        if (backwardSearch) // after calculating the heuristic for astar we change it to the real value
+        {
+            var missingState = new ReGoapState();
+            h = goal.MissingDifference(state, ref missingState);
+            goal = missingState;
+        }
+        else
+        {
+            h = goal.MissingDifference(state);
+        }
+    }
+
+    public int GetPathCost()
+    {
+        return g;
+    }
+
+    public int GetHeuristicCost()
+    {
+        return h;
+    }
+
+    public ReGoapState GetState()
+    {
+        return state;
+    }
+
+    public IEnumerator<IReGoapAction> GetPossibleActionsEnumerator()
+    {
+        var agent = planner.GetCurrentAgent();
+        var actions = agent.GetActionsSet();
+        for (var index = 0; index < actions.Count; index++)
+        {
+            var possibleAction = actions[index];
+            var precond = possibleAction.GetPreconditions(goal);
+            var effects = possibleAction.GetEffects(goal);
+            if (possibleAction == action || !possibleAction.CheckProceduralCondition(agent, goal))
+                continue;
+            if (backwardSearch)
+            {
+                if (effects.HasAny(goal) && // any effect is the current goal
+                    !goal.HasAnyConflict(effects) && // no effect is conflicting with the goal
+                    !goal.HasAnyConflict(precond)) // no precondition is conflicting with the goal
+                    yield return possibleAction;
+            }
+            else
+            {
+                if (precond.MissingDifference(state, 1) == 0 && // check precondition is validated
+                    !goal.HasAnyConflict(effects)) // no effect is conflicting with the goal
+                    yield return possibleAction;
+            }
+        }
+    }
+
+    public List<INode<ReGoapState>> Expand()
+    {
+        var result = new List<INode<ReGoapState>>();
+        var possibleActions = GetPossibleActionsEnumerator();
+        while (possibleActions.MoveNext())
+        {
+            var newGoal = goal;
+            result.Add(
+                new ReGoapNode(
+                    planner,
+                    newGoal,
+                    this,
+                    possibleActions.Current));
+        }
+        return result;
+    }
+
+    public Queue<IReGoapAction> CalculateGoalPath()
+    {
+        var result = new List<IReGoapAction>();
+        var node = this;
+        while (node != null)
+        {
+            if (node.GetAction() != null) //first
+                result.Add(node.GetAction());
+            node = (ReGoapNode)node.GetParent();
+        }
+        // we need to order path in backward search
+        if (backwardSearch)
+        {
+            var orderedResults = new List<IReGoapAction>(result.Count);
+            var memory = (ReGoapState)planner.GetCurrentAgent().GetMemory().GetWorldState().Clone();
+            while (orderedResults.Count < result.Count)
+            {
+                var index = -1;
+                for (int i = 0; i < result.Count; i++)
+                {
+                    var action = result[i];
+                    if (action.GetPreconditions(goal).MissingDifference(memory) == 0)
+                    {
+                        foreach (var effectsPair in action.GetEffects(goal).GetValues())
+                        {
+                            memory.Set(effectsPair.Key, effectsPair.Value);
+                        }
+                        orderedResults.Add(action);
+                        index = i;
+                    }
+                }
+                if (index == -1)
+                    throw new Exception("[ReGoapNode] Error with plan, could not order it.");
+                result.RemoveAt(index);
+            }
+            result = orderedResults;
+            //planner.GetCurrentGoal().GetGoalState().MissingDifference(memory, 1) == 0;
+        }
+        else
+        {
+            result.Reverse();
+        }
+        return new Queue<IReGoapAction>(result);
+    }
+
+    private IReGoapAction GetAction()
+    {
+        return action;
+    }
+
+    public List<INode<ReGoapState>> CalculatePath()
+    {
+        var result = new List<INode<ReGoapState>>();
+        var node = (INode<ReGoapState>)this;
+        while (node.GetParent() != null)
+        {
+            result.Add(node);
+            node = node.GetParent();
+        }
+        result.Reverse();
+        return result;
+    }
+
+    public int CompareTo(INode<ReGoapState> other)
+    {
+        return cost - other.GetCost();
+    }
+
+    public int GetCost()
+    {
+        return cost;
+    }
+
+    public INode<ReGoapState> GetParent()
+    {
+        return parent;
+    }
+
+    public bool IsGoal(ReGoapState goal)
+    {
+        return h == 0;
+    }
+
+    public int Priority { get; set; }
+    public long InsertionIndex { get; set; }
+    public int QueueIndex { get; set; }
+}
---
+++ ReGoapState.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+
+public class ReGoapState : ICloneable
+{
+    // can change to object
+    private volatile Dictionary<string, object> values;
+
+    public ReGoapState(ReGoapState old)
+    {
+        lock (old.values)
+            values = new Dictionary<string, object>(old.values);
+    }
+
+    public ReGoapState()
+    {
+        values = new Dictionary<string, object>();
+    }
+
+    public static ReGoapState operator +(ReGoapState a, ReGoapState b)
+    {
+        ReGoapState result;
+        lock (a.values)
+        {
+            result = new ReGoapState(a);
+        }
+        lock (b.values)
+        {
+            foreach (var pair in b.values)
+                result.values[pair.Key] = pair.Value;
+            return result;
+        }
+    }
+
+    public int Count
+    {
+        get { return values.Count; }
+    }
+    public bool HasAny(ReGoapState other)
+    {
+        lock (values) lock (other.values)
+        {
+            foreach (var pair in other.values)
+            {
+                object thisValue;
+                values.TryGetValue(pair.Key, out thisValue);
+                var otherValue = pair.Value;
+                if (thisValue == otherValue || (thisValue != null && thisValue.Equals(pair.Value)))
+                    return true;
+            }
+            return false;
+        }
+    }
+    public bool HasAnyConflict(ReGoapState other, bool backwardSearch = true) // used only in backward for now
+    {
+        lock (values) lock (other.values)
+        {
+            foreach (var pair in other.values)
+            {
+                object thisValue;
+                values.TryGetValue(pair.Key, out thisValue);
+                var otherValue = pair.Value;
+                // ex. this["isAt"] = "enemy" and other["isAt"] = "base"
+                if (backwardSearch && otherValue.Equals(false)) // backward search does NOT support false preconditions
+                    continue;
+                if (thisValue != null && !otherValue.Equals(thisValue))
+                    return true;
+            }
+            return false;
+        }
+    }
+
+    public int MissingDifference(ReGoapState other, int stopAt = int.MaxValue)
+    {
+        ReGoapState nullGoap = null;
+        return MissingDifference(other, ref nullGoap, stopAt);
+    }
+
+    // write differences in "difference"
+    public int MissingDifference(ReGoapState other, ref ReGoapState difference, int stopAt = int.MaxValue, Func<KeyValuePair<string, object>, object, bool> predicate = null)
+    {
+        lock (values)
+        {
+            var count = 0;
+            foreach (var pair in values)
+            {
+                var add = false;
+                var valueBool = pair.Value as bool?;
+                object otherValue;
+                other.values.TryGetValue(pair.Key, out otherValue);
+                if (valueBool.HasValue)
+                {
+                    // we don't need to check otherValue type since every key is supposed to always have same value type
+                    var otherValueBool = otherValue == null ? false : (bool)otherValue;
+                    if (valueBool.Value != otherValueBool)
+                        add = true;
+                }
+                else // generic version
+                {
+                    if (!pair.Value.Equals(otherValue))
+                        add = true;
+                }
+                if (add && (predicate == null || predicate(pair, otherValue)))
+                {
+                    count++;
+                    if (difference != null)
+                        difference.values[pair.Key] = pair.Value;
+                    if (count >= stopAt)
+                        break;
+                }
+            }
+            return count;
+        }
+    }
+    public object Clone()
+    {
+        var clone = new ReGoapState(this);
+        return clone;
+    }
+
+    public override string ToString()
+    {
+        lock (values)
+        {
+            var result = "GoapState: ";
+            foreach (var pair in values)
+                result += string.Format("'{0}': {1}, ", pair.Key, pair.Value);
+            return result;
+        }
+    }
+
+    public T Get<T>(string key)
+    {
+        lock (values)
+        {
+            if (!values.ContainsKey(key))
+                return default(T);
+            return (T)values[key];
+        }
+    }
+
+    public void Set<T>(string key, T value)
+    {
+        lock (values)
+        {
+            values[key] = value;
+        }
+    }
+
+    public void Remove(string key)
+    {
+        lock (values)
+        {
+            values.Remove(key);
+        }
+    }
+
+    public Dictionary<string, object> GetValues()
+    {
+        lock (values)
+            return values;
+    }
+
+    public bool HasKey(string key)
+    {
+        lock (values)
+            return values.ContainsKey(key);
+    }
+}
